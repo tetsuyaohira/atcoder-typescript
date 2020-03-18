75a630778d407cf35c31ffabe8133d8d7a1cf2f7
tetsuya <tetsuya.ohira@icloud.com>
2020-03-18 21:30:35 +0900

    A-Train.ts

---
+++ src/abc107/A-Train.ts
@@ -0,0 +1,22 @@
+import * as fs from 'fs';
+
+namespace AtCoder.abc007 {
+
+    class Program {
+        /**
+         * in
+         *  4 2
+         * out
+         *  3
+         */
+        main(inp: string): void {
+            const [n,i] :number[] = inp.split(' ').map(Number);
+            console.log(n-i+1);
+        }
+    }
+
+    const prg = new Program();
+    prg.main(fs.readFileSync('/dev/stdin', 'utf8').trim());
+}
+
+
