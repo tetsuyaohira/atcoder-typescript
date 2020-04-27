73045dea01f49fb90c1fd2935dc21b42dd101bf7
tetsuyaohira <tetsuya.ohira@icloud.com>
2020-04-28 00:23:24 +0900

    template.ts

--- src/template.ts
+++ src/template.ts
@@ -1,23 +1,26 @@
 import * as fs from 'fs';
 
 namespace AtCoder {
-    export interface Task {
+    export interface Paramable {
         readonly args: string[][];
+    }
 
+    export interface Task {
         main(this: Task): void;
     }
 }
 
 namespace AtCoder.abc001 {
-    export class TaskA implements AtCoder.Task {
-        constructor(public readonly args: string[][]) {
+    export class TaskA implements AtCoder.Task, AtCoder.Paramable {
+        public constructor(public readonly args: string[][]) {
         }
 
-        main(this: AtCoder.Task): void {
+        public main(this: AtCoder.Task): void {
         }
     }
+
+    const args: string[][] = fs.readFileSync('/dev/stdin', 'utf8').trim().split('\n').map(r => r.split(' '));
+    const task: TaskA = new TaskA(args);
+    task.main();
 }
 
-const args: string[][] = fs.readFileSync('/dev/stdin', 'utf8').trim().split('\n').map(r => r.split(' '));
-const prg: AtCoder.abc001.TaskA = new AtCoder.abc001.TaskA(args);
-prg.main();
