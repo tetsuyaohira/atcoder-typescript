4fb6221c22215292a1ece5e6ad4a8176334027ae
tetsuyaohira <tetsuya.ohira@icloud.com>
2020-05-22 20:02:21 +0900

    A-OneOutOfThree.ts

---
+++ src/abc075/A-OneOutOfThree.ts
@@ -0,0 +1,35 @@
+import * as fs from 'fs';
+
+namespace AtCoder {
+    export interface Paramable {
+        readonly args: string[][];
+    }
+
+    export interface Task {
+        main(this: Task): void;
+    }
+}
+
+namespace AtCoder.abc001 {
+    export class TaskA implements AtCoder.Task, AtCoder.Paramable {
+        public constructor(public readonly args: string[][]) {
+        }
+
+        public main(this: TaskA): void {
+            const abc: number[] = this.args[0].map(Number);
+
+            abc.sort();
+
+            if (abc[0]===abc[1]) {
+                console.log(abc[2]);
+            } else{
+                console.log(abc[0]);
+            }
+        }
+    }
+
+    const args: string[][] = fs.readFileSync('/dev/stdin', 'utf8').trim().split('\n').map(r => r.split(' '));
+    const task: AtCoder.Task = new TaskA(args);
+    task.main();
+}
+
