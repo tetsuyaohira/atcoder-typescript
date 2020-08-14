70f812a5963b57196f87c9acf657c1191255cdc1
tetsuyaohira <tetsuya.ohira@icloud.com>
2020-08-14 22:44:29 +0900

    Formatting

--- src/abc069/A-K-City.ts
+++ src/abc069/A-K-City.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+
 const [n, m]: number[] = fs.readFileSync('/dev/stdin', 'utf8').trim().split(' ').map(Number);
-const ans :number = (n - 1) * (m - 1)
+const ans: number = (n - 1) * (m - 1)
 console.log(ans);
--- src/abc070/A-PalindromicNumber.ts
+++ src/abc070/A-PalindromicNumber.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
-const args : string = fs.readFileSync('/dev/stdin','utf8').trim();
+
+const args: string = fs.readFileSync('/dev/stdin', 'utf8').trim();
 if (args[0] === args[2]) {
     console.log('Yes');
 } else {
