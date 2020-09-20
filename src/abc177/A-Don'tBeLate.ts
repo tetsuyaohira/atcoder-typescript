e39e003f7c18b765d177d7c356e32b8185c5dd12
tetsuyaohira <tetsuya.ohira@icloud.com>
2020-09-20 22:30:48 +0900

    update

--- src/abc177/A-Don'tBeLate.ts
+++ src/abc177/A-Don'tBeLate.ts
@@ -2,4 +2,4 @@ import * as fs from 'fs';
 
 
 const [d, t, s]: number[] = fs.readFileSync('/dev/stdin', 'utf8').trim().split(' ').map(Number);
-console.log((d / s < t) ? 'Yes' : 'No');
+console.log((d / s <= t) ? 'Yes' : 'No');
