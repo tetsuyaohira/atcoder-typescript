3f39d8f9c67d725ebde00a37c3f7ddd5c51579e9
tetsuyaohira <tetsuya.ohira@icloud.com>
2020-11-03 23:53:44 +0900

    B-TrapezoidSum.ts

--- src/abc181/B-TrapezoidSum.ts
+++ src/abc181/B-TrapezoidSum.ts
@@ -6,13 +6,7 @@ const n: number = +args[0];
 let ans: number = 0;
 for (let i = 1; i <= n; i++) {
     const a: number[] = args[i].split(' ').map(Number);
-
-    for (let ii = a[0]; ii <= a[1]; ii++) {
-        ans+=ii;
-    }
-    // ans += a.reduce((previosValue, currentValue) => {
-    //     return previosValue + currentValue;
-    // }, 0);
+    ans += (a[1] - a[0] + 1) * (a[0] + a[1]) / 2;
 }
 console.log(ans);
 
