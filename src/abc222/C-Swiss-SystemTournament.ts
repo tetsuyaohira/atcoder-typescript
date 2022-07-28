b79293e0b0f97b6b2345378bae0cf4e14c283f62
tetsuyaohira <tetsuya.ohira@icloud.com>
2022-07-28 21:34:01 +0900

    C-Swiss-SystemTournament.ts

--- src/abc222/C-Swiss-SystemTournament.ts
+++ src/abc222/C-Swiss-SystemTournament.ts
@@ -15,58 +15,25 @@ for (let i = 0; i < n * 2; i++) {
     wk.push({gcp, lost, name, rank})
 }
 
-
 for (let i: number = 0; i < m; i++) {
-    // console.log(`---m:${i}--------------------------------`);
 
     for (let j: number = 0; j < n; j++) {
-        // console.log(`---n:${j}----------------`);
-
         const gcp1 = wk[j * 2].gcp.split('')[i];
-        // const lost1 = wk[j * 2].lost;
-        // const name1 = wk[j * 2].name;
-
         const gcp2 = wk[j * 2 + 1].gcp.split('')[i];
-        // const lost2 = wk[j * 2 + 1].lost;
-        // const name2 = wk[j * 2 + 1].name;
 
-        // console.log(gcp1, lost1, name1);
-        // console.log(gcp2, lost2, name2);
-
-        if (gcp1 === 'G' && gcp2 === 'C') {
-            // wk[j * 2 + 1].lost++;
+        const ans = vs(gcp1, gcp2);
+        if (ans === 1) {
             wk[j * 2].rank--;
-        }
-        if (gcp1 === 'C' && gcp2 === 'P') {
-            // wk[j * 2 + 1].lost++;
-            wk[j * 2].rank--;
-        }
-        if (gcp1 === 'P' && gcp2 === 'G') {
-            // wk[j * 2].lost++;
+        } else if (ans === -1) {
             wk[j * 2 + 1].rank--;
         }
-
-        if (gcp2 === 'G' && gcp1 === 'C') {
-            // wk[j * 2 + 1].lost++;
-            wk[j * 2].rank--;
-        }
-        if (gcp2 === 'C' && gcp1 === 'P') {
-            // wk[j * 2].lost++;
-            wk[j * 2 + 1].rank--;
-        }
-        if (gcp2 === 'P' && gcp1 === 'G') {
-            // wk[j * 2].lost++;
-            wk[j * 2 + 1].rank--;
-        }
-
-        // console.log(wk);
-
     }
+
     wk = wk.sort((rec1, rec2) => {
         const lost1 = ('000' + rec1.rank.toString()).slice(-3);
         const lost2 = ('000' + rec2.rank.toString()).slice(-3);
-        const name1 = ('00' + rec1.name.toString()).slice(-2);
-        const name2 = ('00' + rec2.name.toString()).slice(-2);
+        const name1 = ('000' + rec1.name.toString()).slice(-3);
+        const name2 = ('000' + rec2.name.toString()).slice(-3);
 
         if ((lost1 + name1) > (lost2 + name2)) {
             return 1;
@@ -76,9 +43,29 @@ for (let i: number = 0; i < m; i++) {
     });
 
 }
-// console.log('===================================================');
-// console.log(wk)
 
 wk.forEach(rec => {
     console.log(rec.name);
 });
+
+function vs(gcp1: string, gcp2: string): number {
+    if (gcp1 === 'G' && gcp2 === 'C') {
+        return 1;
+    }
+    if (gcp1 === 'G' && gcp2 === 'P') {
+        return -1;
+    }
+    if (gcp1 === 'C' && gcp2 === 'G') {
+        return -1;
+    }
+    if (gcp1 === 'C' && gcp2 === 'P') {
+        return 1;
+    }
+    if (gcp1 === 'P' && gcp2 === 'G') {
+        return 1;
+    }
+    if (gcp1 === 'P' && gcp2 === 'C') {
+        return -1;
+    }
+    return 0;
+}
