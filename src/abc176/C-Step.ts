e6732bb86ef2c921df3fb71b3d60d00a0bdabd68
tetsuyaohira <tetsuya.ohira@icloud.com>
2022-10-01 07:34:05 +0900

    C-Step.ts

--- src/abc176/C-Step.ts
+++ src/abc176/C-Step.ts
@@ -5,3 +5,15 @@ const input = readFileSync(0, "utf8").trim().split("\n")
 const n: number = +input[0]
 const a: number[] = input[1].split(" ").map(Number)
 
+let ans: number = 0;
+let max:number = 0
+for (let i = 0; i < n; i++) {
+    let wk:number = a[i]
+    max = Math.max(max,wk)
+
+    if (wk < max){
+        ans += max -wk
+    }
+}
+
+console.log(ans)
